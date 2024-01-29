1a7e093adeb3cbfeb7ea0b5efd45aa4a9f1be0e0
bpdeveloperscommunity <bepracticaldeveloperscommunity@gmail.com>
2024-01-29 12:48:10 +0530

    updated files

--- src/components/Homepage/Home.jsx
+++ src/components/Homepage/Home.jsx
@@ -67,12 +67,30 @@ function Home() {
         <div className="row">
             {
                  data.map((item, index) => (
-                   <div className="col-6 col-sm-6 col-md-3 col-lg-2 mb-3">
-                     <div className="job-post bg-white d-flex flex-column gap-2 rounded-2 align-items-center text-center p-2 h-100 justify-content-between" key={index}>
-                     <img src="https://cdn-icons-png.flaticon.com/128/3688/3688596.png" alt="" />
-                     <small className='small text-info'>{item.companyName}</small>
-                    <a href={`/jobview/${item._id}`}> <h1 className="fs-5">{item.role}</h1></a>
-                    </div>
+                   <div className="col-12 col-sm-12 col-md-6 mb-3">
+                    <div className="job-header p-3 bg-white rounded-3 mt-3">
+            <span className="d-block fs-5 fw-bold">{item.role}</span>
+            <span className="text-secondary small">{item.companyName}</span>
+            <div className="d-flex gap-3 mt-3 ">
+            <span className="d-block  text-secondary"><i class="bi bi-wallet2  text-secondary"></i> 0-1 years</span>
+            <span className="d-block  text-secondary">&#8377;{item.package}</span>
+            </div>
+            <div className="d-flex gap-2 mt-3">
+            <i class="bi bi-geo-alt"></i>
+            <span className='fs-6 text-secondary'>Bangalore Rural,Karnataka, Bengaluru,Karnataka( Richmond Town, Domlur, Indira Nagar, 1A Block Koramangala, HSR Layout, BTM Layout, Manayata Tech Park, Marathahalli, Rajaji Nagar )</span>
+            </div>
+            <hr />
+            <div className="d-flex gap-3 align-items-center justify-content-between flex-wrap">
+            <div className="d-flex gap-3">
+                <span className="text-secondary small">Posted: <span className="text-black">1 day ago</span></span>
+                <span className="text-secondary small">Openings: <span className="text-black">6</span></span>
+            </div>
+        <div className="d-flex gap-2 ">
+        <Button variant='contained' className='p-2 rounded-5 px-4'>Apply for job</Button>
+        <Button variant='outlined' className='p-2 rounded-5 px-4'>Learn More</Button>
+        </div>
+            </div>
+        </div>
                    </div>
                   ))
             }
